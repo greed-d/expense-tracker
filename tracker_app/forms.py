452d7daafd9c6baa702b9694aae6401ec7a51568
greed-d <deekshit.bti@gmail.com>
2024-12-20 17:36:21 +0545

    `chore` : change position of email field

--- tracker_app/forms.py
+++ tracker_app/forms.py
@@ -10,9 +10,9 @@ class UserRegsiterForm(UserCreationForm):
             "username",
             "first_name",
             "last_name",
+            "email",
             "password1",
             "password2",
-            "email",
         ]
 
 
