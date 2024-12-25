4ddd93ac083c43570601fd2fb58235cea4dd2353
greed-d <deekshit.bti@gmail.com>
2024-12-25 16:52:02 +0545

    `chore` : change herosection to default page

--- tracker_app/urls.py
+++ tracker_app/urls.py
@@ -1,10 +1,10 @@
-from django.contrib import admin
 from django.urls import path
 
 from tracker_app import views
 
 urlpatterns = [
     path("user_register/", views.UserRegisterView.as_view(), name="user_register"),
-    path("", views.UserLoginStuff.as_view(), name="login"),
+    path("login/", views.UserLoginStuff.as_view(), name="login"),
     path("home/", views.UserDashboardView.as_view(), name="home"),
+    path("", views.HeroSectionView.as_view(), name="hero"),
 ]
