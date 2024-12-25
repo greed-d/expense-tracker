b413fa744ccbf183d1327f440bfe76d3ec52336e
greed-d <deekshit.bti@gmail.com>
2024-12-25 16:51:50 +0545

    `chore` : saved in db and model fixups
    - Changed how texts are stored in Models
    - Changed FloatField to DecimalField to remove rounding errors

--- tracker_app/models.py
+++ tracker_app/models.py
@@ -1,37 +1,37 @@
-from django.core.validators import BaseValidator
-from django.db import models
 from django.contrib.auth.models import User
+from django.db import models
 
-
-INCOME_AT = [("Bank", "Bank"), ("Wallet", "Wallet"), ("Cash", "Cash")]
-INCOME_REASON = [("Bank", "Bank"), ("Wallet", "Wallet"), ("Cash", "Cash")]
+INCOME_AT = [("BA", "Bank"), ("WA", "Wallet"), ("CA", "Cash")]
+INCOME_REASON = []
 
 EXPENSE_FROM = [("Bank", "Bank"), ("Wallet", "Wallet"), ("Cash", "Cash")]
-EXPENSE_REASON = [
-    ("Bank", "Bank-reason"),
-    ("Wallet", "Wallet-reason"),
-    ("Cash", "Cash-reason"),
-]
 
 
 class IncomeTracker(models.Model):
+    amount = models.DecimalField(max_digits=12, default=0, decimal_places=2)
     source = models.CharField(max_length=50, choices=INCOME_AT)
-    amount = models.FloatField()
-    reason = models.CharField(max_length=50, choices=INCOME_REASON)
+    reason = models.CharField(max_length=50)
     remarks = models.TextField()
-    time = models.DateTimeField()
+    time = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.reason
 
 
+# class UserBalance(models.Model):
+#     current_amount = models.DecimalField(max_digits=12, default=0, decimal_places=2)
+#     income_amount = models.DecimalField(max_digits=12, default=0, decimal_places=2)
+#     expense_amount = models.DecimalField(max_digits=12, default=0, decimal_places=2)
+#     user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
 class ExpenseTracker(models.Model):
+    amount = models.DecimalField(max_digits=12, default=0, decimal_places=2)
     source = models.CharField(max_length=50, choices=EXPENSE_FROM)
-    amount = models.FloatField()
-    reason = models.CharField(max_length=50, choices=EXPENSE_REASON)
+    reason = models.CharField(max_length=200)
     remarks = models.TextField()
-    time = models.DateTimeField()
+    time = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
@@ -39,9 +39,6 @@ class ExpenseTracker(models.Model):
 
 
 class ExpenseCategory(models.Model):
+    # default = ["Entertainment", "Transportation", "Food"]
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    name = models.CharField(max_length=15)
-
-
-class IncomeCategory(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=20)
