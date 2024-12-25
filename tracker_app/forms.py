ff92bdcb13e0c3925f9a2a079179341e443b75f4
greed-d <deekshit.bti@gmail.com>
2024-12-25 16:55:58 +0545

    `chore` : changed from ModelForm to forms.Form

--- tracker_app/forms.py
+++ tracker_app/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from tracker_app.models import IncomeTracker, ExpenseTracker
 
 
 class UserRegsiterForm(UserCreationForm):
@@ -15,7 +16,45 @@ class UserRegsiterForm(UserCreationForm):
             "password2",
         ]
 
-
 class UserLoginForm(forms.Form):
     username = forms.CharField(required=True, max_length=20)
     password = forms.CharField(widget=forms.PasswordInput, required=True)
+
+class InputOrExpense(forms.Form):
+    input_type = forms.ChoiceField(
+        choices=(("IN", "Income"), ("EX", "Expense")),
+        widget=forms.Select,
+        required=True
+    )
+    amount = forms.DecimalField(
+        max_digits=12, 
+        decimal_places=2,
+        min_value=1,  # Ensure no negative amounts are entered
+        required=True
+    )
+    source = forms.ChoiceField(
+        choices=(("CA", "Cash"), ("BA", "Bank"), ("WA", "Wallet")),
+        widget=forms.Select,
+        required=True
+    )
+    reason = forms.CharField(
+        max_length=200, 
+        required=True
+    )
+    remarks = forms.CharField(
+        widget=forms.Textarea(attrs={"placeholder": "Enter remarks (optional)"} ), 
+        required=False
+    )
+
+
+
+class IncomeInputForm(forms.ModelForm):
+    class Meta:
+        model = IncomeTracker
+        fields = ["amount", "source", "reason", "remarks"]
+
+
+class ExpenseInputForm(forms.ModelForm):
+    class Meta:
+        model = ExpenseTracker
+        fields = ["amount", "source", "reason", "remarks"]
