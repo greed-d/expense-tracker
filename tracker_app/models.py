22e65ca07f14059deca94f2aff3aa9d188d46e96
greed-d <deekshit.bti@gmail.com>
2024-12-26 17:16:44 +0545

    `chore` : cleanup

--- tracker_app/models.py
+++ tracker_app/models.py
@@ -6,6 +6,13 @@ INCOME_REASON = []
 
 EXPENSE_FROM = [("Bank", "Bank"), ("Wallet", "Wallet"), ("Cash", "Cash")]
 
+class ExpenseCategory(models.Model):
+    user = models.ForeignKey(User,blank=True, null=True, on_delete=models.CASCADE )
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return f"{self.name}"
+
 
 class IncomeTracker(models.Model):
     amount = models.DecimalField(max_digits=12, default=0, decimal_places=2)
@@ -16,29 +23,20 @@ class IncomeTracker(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.reason
+        return f"{self.source} - {self.amount}"
 
-
-# class UserBalance(models.Model):
-#     current_amount = models.DecimalField(max_digits=12, default=0, decimal_places=2)
-#     income_amount = models.DecimalField(max_digits=12, default=0, decimal_places=2)
-#     expense_amount = models.DecimalField(max_digits=12, default=0, decimal_places=2)
-#     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
 
 class ExpenseTracker(models.Model):
     amount = models.DecimalField(max_digits=12, default=0, decimal_places=2)
     source = models.CharField(max_length=50, choices=EXPENSE_FROM)
+    category = models.ForeignKey(ExpenseCategory, on_delete=models.SET_NULL, null=True)
     reason = models.CharField(max_length=200)
     remarks = models.TextField()
     time = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.reason
+        return f"{self.category} - {self.amount}"
 
 
-class ExpenseCategory(models.Model):
-    # default = ["Entertainment", "Transportation", "Food"]
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    name = models.CharField(max_length=20)
