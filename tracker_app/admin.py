a9d94896f355187cbb8a67ff137f54f4d0105f60
greed-d <deekshit.bti@gmail.com>
2024-12-26 17:15:21 +0545

    `add` : add `ExpenseCategory` to admin panel

--- tracker_app/admin.py
+++ tracker_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from tracker_app.models import ExpenseTracker, IncomeTracker
+from tracker_app.models import ExpenseCategory, ExpenseTracker, IncomeTracker
 
 admin.site.register(IncomeTracker)
 admin.site.register(ExpenseTracker)
+admin.site.register(ExpenseCategory)
