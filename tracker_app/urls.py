2f7a7944660a1a7eab113e5ef22bea0691beb185
greed-d <deekshit.bti@gmail.com>
2024-12-30 16:42:14 +0545

    `chore` : add filter for testing

--- tracker_app/urls.py
+++ tracker_app/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 
-from tracker_app import views
+from tracker_app.views.dashboard import UserDashboardView
+from tracker_app.views.landing import HeroSectionView
+from tracker_app.views.login_register import UserLoginView, UserRegisterView
 
 urlpatterns = [
-    path("user_register/", views.UserRegisterView.as_view(), name="user_register"),
-    path("login/", views.UserLoginStuff.as_view(), name="login"),
-    path("home/", views.UserDashboardView.as_view(), name="home"),
-    path("", views.HeroSectionView.as_view(), name="hero"),
+    path("user_register/", UserRegisterView.as_view(), name="user_register"),
+    path("login/", UserLoginView.as_view(), name="login"),
+    # path("filter/", FilterFormView.as_view(), name="filter"),
+    path("home/", UserDashboardView.as_view(), name="home"),
+    path("", HeroSectionView.as_view(), name="hero"),
 ]
