707d988b1ba492a30badc5af75dc0b0332c422bc
greed-d <deekshit.bti@gmail.com>
2024-12-30 18:01:14 +0545

    `feat` : submit according to form

--- tracker_app/views/dashboard.py
+++ tracker_app/views/dashboard.py
@@ -12,31 +12,70 @@ class UserDashboardView(LoginRequiredMixin, View):
     form_class = InputOrExpense
     filter_result = None
 
-    def get(self, request):
-        total_income = IncomeTracker.objects.filter(user=request.user).aggregate(models.Sum("amount"))["amount__sum"] or 0
-        total_expense = ExpenseTracker.objects.filter(user=request.user).aggregate(models.Sum("amount"))["amount__sum"] or 0
+    def get_data(self, user):
+        total_income = IncomeTracker.objects.filter(user=user).aggregate(models.Sum("amount"))["amount__sum"] or 0
+        total_expense = ExpenseTracker.objects.filter(user=user).aggregate(models.Sum("amount"))["amount__sum"] or 0
 
 
         expense_category = (
-        ExpenseTracker.objects.filter(user=request.user)
+        ExpenseTracker.objects.filter(user=user)
             .values("category__name")
             .annotate(Total_amount=models.Sum("amount"))
         )
 
         expense_source = (
-            ExpenseTracker.objects.filter(user=request.user)
+            ExpenseTracker.objects.filter(user=user)
             .values("source")
             .annotate(total_sum=models.Sum("amount"))
         )
         #
         income_source = (
-            IncomeTracker.objects.filter(user=request.user)
+            IncomeTracker.objects.filter(user=user)
             .values("source")
             .annotate(total_sum=models.Sum("amount"))
         )
 
 
         current_balance = total_income - total_expense
+
+        return {
+            "current_balance" : current_balance,
+            "total_income" : total_income,
+            "total_expense" : total_expense,
+            "expense_source" : expense_source,
+            "income_source" : income_source,
+            "expense_category": expense_category
+            # "filter_result": filter_result,
+            # "expense_category" : expense_category,
+        }
+
+
+    def get(self, request):
+        context_data = self.get_data(request.user)
+        # total_income = IncomeTracker.objects.filter(user=request.user).aggregate(models.Sum("amount"))["amount__sum"] or 0
+        # total_expense = ExpenseTracker.objects.filter(user=request.user).aggregate(models.Sum("amount"))["amount__sum"] or 0
+        #
+        #
+        # expense_category = (
+        # ExpenseTracker.objects.filter(user=request.user)
+        #     .values("category__name")
+        #     .annotate(Total_amount=models.Sum("amount"))
+        # )
+        #
+        # expense_source = (
+        #     ExpenseTracker.objects.filter(user=request.user)
+        #     .values("source")
+        #     .annotate(total_sum=models.Sum("amount"))
+        # )
+        # #
+        # income_source = (
+        #     IncomeTracker.objects.filter(user=request.user)
+        #     .values("source")
+        #     .annotate(total_sum=models.Sum("amount"))
+        # )
+        #
+        #
+        # current_balance = total_income - total_expense
 
 
         # Get form for filtering 
@@ -46,30 +85,26 @@ class UserDashboardView(LoginRequiredMixin, View):
         #Debug Expressions
         print()
         print("------------------------------------")
-        print(f"TOTAL_INCOME: {total_income}")
-        print(f"TOTAL_EXPENSE: {total_expense}")
-        print(f"TOTAL_EXPENSE: {expense_source}")
-        print(f"INCOME SOURCE: {income_source}")
-        print(f"CURRENT_BALANCE: {current_balance}")
-        print(f"EXPENSE_CATEGORY : {expense_category}")
+        # print(f"TOTAL_INCOME: {total_income}")
+        # print(f"TOTAL_EXPENSE: {total_expense}")
+        # print(f"TOTAL_EXPENSE: {expense_source}")
+        # print(f"INCOME SOURCE: {income_source}")
+        # print(f"CURRENT_BALANCE: {current_balance}")
+        # print(f"EXPENSE_CATEGORY : {expense_category}")
         # print(f"SOURCE : {self.form_class(request.POST).cleaned_data['source']}")
         print("------------------------------------")
         print()
 
         context = {
-            "current_balance" : current_balance,
-            "total_income": total_income,
-            "total_expense": total_expense,
-            "expense_total" : expense_source,
             "filter_form": filter_form,
-            # "filter_result": filter_result,
-            # "expense_category" : expense_category,
             "form": InputOrExpense(user = request.user),
+            **context_data
         }
 
         return render(request, self.template_name, context)
 
     def post(self, request):
+        context_data = self.get_data(request.user)
         form = self.form_class(request.POST)
         filter_form = SourceFilterForm(request.POST)
 
@@ -78,65 +113,67 @@ class UserDashboardView(LoginRequiredMixin, View):
         print(request.POST)
 
         # breakpoint()
-        if "tracker_submit" in request.POST and form.is_valid():
-            input_type = form.cleaned_data["input_type"]
-            amount = form.cleaned_data["amount"]
-            source = form.cleaned_data["source"]
-            time = form.cleaned_data["time"]
-            reason = form.cleaned_data["reason"]
-            remarks = form.cleaned_data["remarks"]
-            category = form.cleaned_data["category"]
+        if "tracker_submit" in request.POST:
+            if form.is_valid():
+                input_type = form.cleaned_data["input_type"]
+                amount = form.cleaned_data["amount"]
+                source = form.cleaned_data["source"]
+                time = form.cleaned_data["time"]
+                reason = form.cleaned_data["reason"]
+                remarks = form.cleaned_data["remarks"]
+                category = form.cleaned_data["category"]
 
 
-            print()
-            print("-------------------------")
-            # print(f"Form is : { form }")
-            print(f"Input Type : {input_type}")
-            print(f"Amount : {amount}")
-            print(f"Source : {source}")
-            print(f"Category : {category}")
-            # print(time)
-            print("-------------------------")
-            print()
+                print()
+                print("-------------------------")
+                # print(f"Form is : { form }")
+                print(f"Input Type : {input_type}")
+                print(f"Amount : {amount}")
+                print(f"Source : {source}")
+                print(f"Category : {category}")
+                # print(time)
+                print("-------------------------")
+                print()
 
-            if input_type == "IN":
-                IncomeTracker.objects.create(
-                    user = self.request.user,
-                    amount = amount,
-                    source = source,
-                    time = time,
-                    reason = reason,
-                    remarks = remarks
-                )
+                if input_type == "IN":
+                    IncomeTracker.objects.create(
+                        user = self.request.user,
+                        amount = amount,
+                        source = source,
+                        time = time,
+                        reason = reason,
+                        remarks = remarks
+                    )
 
-            elif input_type == "EX":
-                # category = form.cleaned_data["category"]
-                ExpenseTracker.objects.create(
-                    user = self.request.user,
-                    amount = amount,
-                    source = source,
-                    time = time,
-                    category = category,
-                    reason = reason,
-                    remarks = remarks,
-                )
-            return redirect("home")
+                elif input_type == "EX":
+                    # category = form.cleaned_data["category"]
+                    ExpenseTracker.objects.create(
+                        user = self.request.user,
+                        amount = amount,
+                        source = source,
+                        time = time,
+                        category = category,
+                        reason = reason,
+                        remarks = remarks,
+                    )
+                return redirect("home")
 
 
-        elif "filter_submit" in request.POST and filter_form.is_valid():
-            search_type = filter_form.cleaned_data["type"]
-            print( search_type )
+        elif "filter_submit" in request.POST :
+            if filter_form.is_valid():
+                search_type = filter_form.cleaned_data["type"]
+                print( search_type )
 
-            if search_type == "IN":
-                selected_source = filter_form.cleaned_data["source"]
-                print(selected_source)
-                filter_result = IncomeTracker.objects.filter(user=request.user, source=selected_source).values_list("amount", flat=True)
-                print(f"Incom filter result {filter_result}")
+                if search_type == "IN":
+                    selected_source = filter_form.cleaned_data["source"]
+                    print(f"selected_source : {selected_source}")
+                    filter_result = IncomeTracker.objects.filter(user=request.user, source=selected_source).values_list("amount", flat=True)
+                    print(f"Incom filter result {filter_result}")
 
-            elif search_type == "EX":
-                selected_source = filter_form.cleaned_data["source"]
-                print(f"Expense Source {selected_source}")
-                filter_result = ExpenseTracker.objects.filter(user=request.user, source=selected_source).values_list("amount", flat=True)
+                elif search_type == "EX":
+                    selected_source = filter_form.cleaned_data["source"]
+                    print(f"Expense Source {selected_source}")
+                    filter_result = ExpenseTracker.objects.filter(user=request.user, source=selected_source).values_list("amount", flat=True)
 
             
         else:
@@ -146,6 +183,13 @@ class UserDashboardView(LoginRequiredMixin, View):
                     print(f"Field '{field}': {error}")
             print("Non-field errors:", form.non_field_errors())
 
-        return redirect("home")
+        context = {
+            "form" : self.form_class(user=request.user),
+            "filter_form" : filter_form,
+            "filter_result" : filter_result,
+            **context_data
+        }
+
+        return render(request, self.template_name, context=context)
 
 
