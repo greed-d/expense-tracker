231b9dbe2e4b49ba972d359be20b15f5d817e02c
greed-d <deekshit.bti@gmail.com>
2024-12-31 15:56:57 +0545

    `feat` : add category feature

---
+++ tracker_app/views/add_category.py
@@ -0,0 +1,31 @@
+from django.shortcuts import redirect, render
+from django.views.generic.base import View
+
+from tracker_app.forms import AddCategory
+from tracker_app.models import ExpenseCategory
+
+
+class AddCategoryView(View):
+    template_name = "tracker_app/add_category.html"
+    form_class = AddCategory
+
+    def get(self, request):
+        form = self.form_class
+        categories = ExpenseCategory.objects.filter(user=request.user)
+
+        context = {
+            "form" : form,
+            "categories": categories
+        }
+        return render(request, self.template_name, context=context)
+
+    def post(self, request):
+        print(f"From add category viewe : {request.POST}")
+        form = self.form_class(request.POST)
+
+        if form.is_valid():
+            category = form.save(commit=False)
+            category.user = request.user
+            category.save()
+            return redirect("addcategory")
+        return render(request, self.template_name, {"form" : form})
