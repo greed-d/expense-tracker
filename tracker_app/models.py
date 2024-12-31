030a3f0f5bb9335be47a908331525f4fabfa1cec
greed-d <deekshit.bti@gmail.com>
2024-12-31 16:02:21 +0545

    `chore` : datetime fixup

--- tracker_app/models.py
+++ tracker_app/models.py
@@ -18,8 +18,9 @@ class IncomeTracker(models.Model):
     amount = models.DecimalField(max_digits=12, default=0, decimal_places=2)
     source = models.CharField(max_length=50, choices=INCOME_AT)
     reason = models.CharField(max_length=50)
+    category = models.ForeignKey(ExpenseCategory, on_delete=models.CASCADE, null=True, blank=True)
     remarks = models.TextField()
-    time = models.DateTimeField(auto_now_add=True)
+    time = models.DateTimeField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
@@ -33,7 +34,7 @@ class ExpenseTracker(models.Model):
     category = models.ForeignKey(ExpenseCategory, on_delete=models.SET_NULL, null=True)
     reason = models.CharField(max_length=200)
     remarks = models.TextField()
-    time = models.DateTimeField(auto_now_add=True)
+    time = models.DateTimeField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
--- tracker_app/urls.py
+++ tracker_app/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 
+from tracker_app.views.add_category import AddCategoryView
 from tracker_app.views.dashboard import UserDashboardView
 from tracker_app.views.landing import HeroSectionView
 from tracker_app.views.login_register import UserLoginView, UserRegisterView
@@ -7,7 +8,7 @@ from tracker_app.views.login_register import UserLoginView, UserRegisterView
 urlpatterns = [
     path("user_register/", UserRegisterView.as_view(), name="user_register"),
     path("login/", UserLoginView.as_view(), name="login"),
-    # path("filter/", FilterFormView.as_view(), name="filter"),
+    path("addcategory/", AddCategoryView.as_view(), name="addcategory"),
     path("home/", UserDashboardView.as_view(), name="home"),
     path("", HeroSectionView.as_view(), name="hero"),
 ]
